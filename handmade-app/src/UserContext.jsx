c5fb1223be05b25b5946a11cfba7c5b9a3c51187
cristiursulescu <ursulescucristian87@outlook.com>
2023-11-22 13:51:17 +0200

    css update

--- handmade-app/src/UserContext.jsx
+++ handmade-app/src/UserContext.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-
 export const UserContext = React.createContext();
 
 export default function UserContextProvider(props) {
--- handmade-app/src/index.js
+++ handmade-app/src/index.js
@@ -1,15 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { createRoot } from "react-dom/client";
-import {
-    createBrowserRouter,
-    RouterProvider,
-    Route,
-    Link,
-} from "react-router-dom";
-
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
